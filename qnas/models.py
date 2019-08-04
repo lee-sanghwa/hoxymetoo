f1b7805d0ded76c65c566fd440df0076e8db90f4
이상화 <joseph@devhi.me>
2019-08-04 23:30:38 +0900

    Update model of qnas with mysql ERD

--- qnas/models.py
+++ qnas/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        db_table = 'Category'
+
+
+class Question(models.Model):
+    qcontent = models.TextField()
+    categoryid = models.IntegerField()
+    cdatetime = models.CharField(max_length=14)
+    cdate = models.CharField(max_length=8)
+    ctime = models.CharField(max_length=6)
+
+    class Meta:
+        db_table = 'Question'
+
+
+class Answer(models.Model):
+    acontent = models.TextField()
+    categoryid = models.IntegerField()
+    cdatetime = models.CharField(max_length=14)
+    cdate = models.CharField(max_length=8)
+    ctime = models.CharField(max_length=6)
+
+    class Meta:
+        db_table = 'Answer'
+
+
+class Qna(models.Model):
+    qid = models.OneToOneField(
+        Question,
+        on_delete=models.CASCADE,
+        db_column='qid'
+    )
+    aid = models.OneToOneField(
+        Answer,
+        on_delete=models.CASCADE,
+        db_column='aid'
+    )
+    cdatetime = models.CharField(max_length=14)
+    cdate = models.CharField(max_length=8)
+    ctime = models.CharField(max_length=6)
+
+    class Meta:
+        db_table = 'Qna'
