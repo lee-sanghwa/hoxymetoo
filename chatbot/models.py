6422d314c056bc54b49bf515d88c9183648cc63d
이상화 <joseph@devhi.me>
2019-08-07 14:11:02 +0900

    Create chatlog model

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from members.models import Member
 
-# Create your models here.
+
+class Chatlog(models.Model):
+    senderid = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='senderid',
+        db_column='senderid'
+    )
+    receiveid = models.ForeignKey(
+        Member,
+        on_delete=models.CASCADE,
+        related_name='receiveid',
+        db_column='receiveid'
+    )
+    content = models.TextField()
+    cdatetime = models.CharField(max_length=14)
+    cdate = models.CharField(max_length=8)
+    ctime = models.CharField(max_length=6)
+
+    class Meta:
+        db_table = 'Chatlog'
