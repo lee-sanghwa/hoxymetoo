f646ac52bb83bcd93a7a1dbe5cc619be864ab2d2
이상화 <joseph@devhi.me>
2019-08-07 14:17:03 +0900

    Create chatbot view and Add for fields that are required but work within the server

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from chatbot.serializers import ChatlogSerializer
+from chatbot.models import Chatlog
+from rest_framework import viewsets
+from datetime import datetime
 
-# Create your views here.
+
+class ChatbotViewSet(viewsets.ModelViewSet):
+    queryset = Chatlog.objects.all()
+    serializer_class = ChatlogSerializer
+
+    def create(self, request, *args, **kwargs):
+        member_data = request.data
+        now_datetime = datetime.now()
+
+        cdatetime = "%04d%02d%02d%02d%02d%02d" % (
+            now_datetime.year,
+            now_datetime.month,
+            now_datetime.day,
+            now_datetime.hour,
+            now_datetime.minute,
+            now_datetime.second
+        )
+        cdate = cdatetime[:8]
+        ctime = cdatetime[8:]
+
+        member_data['cdatetime'] = cdatetime
+        member_data['cdate'] = cdate
+        member_data['ctime'] = ctime
+
+        super().create(request, *args, **kwargs)
