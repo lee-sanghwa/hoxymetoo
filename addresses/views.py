9c9d0fb1b715a86df1382e5ad12f685922c69a70
이상화 <joseph@devhi.me>
2019-09-25 20:10:28 +0900

    각 시도에 속한 시군구에 대한 정보를 얻기위해 파라미터 추가 및 처리

--- addresses/views.py
+++ addresses/views.py
@@ -1,5 +1,6 @@
 from addresses.models import SiDo, SiGunGu, Address
 from addresses.serializers import SiDoSerializer, SiGunGuSerializer, AddressSerializer
+from rest_framework.mixins import ListModelMixin
 from rest_framework import viewsets
 
 
@@ -11,6 +12,27 @@ class SiDoViewSet(viewsets.ModelViewSet):
 class SiGunGuViewSet(viewsets.ModelViewSet):
     queryset = SiGunGu.objects.all()
     serializer_class = SiGunGuSerializer
+
+    def list(self, request, *args, **kwargs):
+        existing_queryset = self.queryset
+        query_dict = request.GET
+
+        si_do_id = query_dict.get('sidoid')
+
+        if si_do_id is not None:
+            select_si_gun_gu_with_si_do_query = """
+            SELECT * FROM Sigungu
+            JOIN Address ON Sigungu.sigunguid = Address.sigunguid
+            WHERE Address.sidoid = (%s)
+            AND Sigungu.siGunGuId != (%s)
+            """
+            bon_cheung_id = '9999999'
+            select_si_gun_gu_with_si_do_query = select_si_gun_gu_with_si_do_query % (si_do_id, bon_cheung_id)
+            existing_queryset = SiGunGu.objects.raw(select_si_gun_gu_with_si_do_query)
+
+        self.queryset = existing_queryset
+
+        return ListModelMixin.list(self, request, *args, **kwargs)
 
 
 class AddressViewSet(viewsets.ModelViewSet):
