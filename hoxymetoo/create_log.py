a6057056299d74a80ba86e7d5cb956aecb238efa
이상화 <joseph@devhi.me>
2019-11-06 21:33:09 +0900

    log파일에 query string도 추가

--- hoxymetoo/create_log.py
+++ hoxymetoo/create_log.py
@@ -5,6 +5,7 @@ def create_log_content(request):
     meta_data_of_request = request.META
     request_info_string = '  ||  USER-IP: ' + meta_data_of_request.get('HTTP_X_FORWARDED_FOR')
     request_info_string += '  ||  PATH_INFO: ' + meta_data_of_request.get('PATH_INFO')
+    request_info_string += '  ||  QUERY_STRING: ' + meta_data_of_request.get('QUERY_STRING')
     request_info_string += '  ||  REQUEST_METHOD: ' + meta_data_of_request.get('REQUEST_METHOD')
     request_info_string += '  ||  HTTP_USER_AGENT:  ' + meta_data_of_request.get('HTTP_USER_AGENT').split(' ')[0]
     request_info_string += '  ||  REQUEST_BODY: ' + json.dumps(dict(request.data))
