914e041a6371205d6c2fba8b3c39bafb9ed95745
이상화 <joseph@devhi.me>
2019-11-15 00:32:14 +0900

    주어진 단어에대해 인덱스가 만들어져있지않다면 복지라는 키워드로 대체

--- chatbot/views.py
+++ chatbot/views.py
@@ -124,9 +124,9 @@ class ChatBotViewSet(viewsets.ModelViewSet):
                 # 사용자의 질문자체의 Noun에 대한 index정보 획득
                 try:
                     recommend_index = Index.objects.get(indexName=key)
-                # 사용자의 질문자체가 인덱스로 등록 안되어있는 경우에 대한 예외처리
+                # 사용자의 질문자체가 인덱스로 등록 안되어있는 경우에 대한 예외처
                 except Index.DoesNotExist:
-                    continue
+                    recommend_index = 1560
                 # 사용자의 질문자체의 Noun에 대한 index를 갖는 복지들 획득
                 list_recommend_welfare_index = WelIndex.objects.filter(indexId=recommend_index)
                 for recommend_welfare_index in list_recommend_welfare_index:
